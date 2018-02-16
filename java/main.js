e1d020b7de551715fa9352fedcaef7a013c78976
SpaceMonkee <joshthebro4@gmail.com>
2018-02-16 13:15:31 +0200

    V3
    Adding textures

--- java/main.js
+++ java/main.js
@@ -2,3 +2,7 @@ var geometry   = new THREE.SphereGeometry(0.5, 32, 32)
 var material  = new THREE.MeshPhongMaterial()
 var earthMesh = new THREE.Mesh(geometry, material)
 scene.add(earthMesh)
+
+// ADDING EARTH TEXTURE
+
+material.map    = THREE.ImageUtils.loadTexture('images/earthmap1k.jpg')
